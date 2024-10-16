187d880436dfce23bd344cce0f1a21f259363c66
Hodo Ismail <ic17oq5i@students.codeinstitute.net>
2024-10-16 18:49:10 +0000

    Add subtraction game

--- assests/js/script.js
+++ assests/js/script.js
@@ -21,10 +21,14 @@ let num2 = Math.floor(Math.random()*25)+1;
 
 if (gameType === "addition"){
     displayAdditionQuestion(num1,num2)
+
 }else if(gameType === "multiply"){
 displayMultiplyQuestion(num1,num2)
 
 
+}else if(gameType === "subtract"){
+displaySubractQuestion(num1,num2)
+    
 }else{
     alert(`Unknown game type: ${gameType}`)
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -56,6 +60,8 @@ if (operator==="+"){
     return [operand1 + operand2, "addition"]
 }else if(operator==="x"){
     return [operand1 * operand2, "multiply"]
+}else if(operator==="-"){
+    return [operand1 - operand2, "subtract"]
 }else{
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -80,8 +86,10 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 
-function displaySubractQuestion(){
-
+function displaySubractQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent= operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent= operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent="-";
 }
 
 function displayMultiplyQuestion(operand1, operand2){
