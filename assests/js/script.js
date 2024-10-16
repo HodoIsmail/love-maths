a8d63aa926bcc7479ae0bd509cd86f2dce91f783
Hodo Ismail <ic17oq5i@students.codeinstitute.net>
2024-10-16 18:25:03 +0000

    Add multiplication game

--- assests/js/script.js
+++ assests/js/script.js
@@ -21,6 +21,10 @@ let num2 = Math.floor(Math.random()*25)+1;
 
 if (gameType === "addition"){
     displayAdditionQuestion(num1,num2)
+}else if(gameType === "multiply"){
+displayMultiplyQuestion(num1,num2)
+
+
 }else{
     alert(`Unknown game type: ${gameType}`)
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -34,8 +38,10 @@ let isCorrect = userAnswer===calculatedAnswer[0];
 
 if(isCorrect){
     alert("Yay! You got it right <3")
+    incrementScore()
 }else{
     alert(`Ahhh.. you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    incrementWrongScore();
 }
 
 runGame(calculatedAnswer[1]);
@@ -46,8 +52,10 @@ let operand1 = parseInt(document.getElementById("operand1").innerText);
 let operand2 = parseInt(document.getElementById("operand2").innerText);
 let operator = document.getElementById("operator").innerText;
 
-if (operator="+"){
+if (operator==="+"){
     return [operand1 + operand2, "addition"]
+}else if(operator==="x"){
+    return [operand1 * operand2, "multiply"]
 }else{
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
@@ -55,11 +63,13 @@ if (operator="+"){
 }
 
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText= ++oldScore;
 }
 
 function incrementWrongScore(){
-
+let oldScore = parseInt(document.getElementById("incorrect").innerText);
+document.getElementById("incorrect").innerText= ++oldScore;
 
 }
 
@@ -74,7 +84,9 @@ function displaySubractQuestion(){
 
 }
 
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="x";
 }
 
